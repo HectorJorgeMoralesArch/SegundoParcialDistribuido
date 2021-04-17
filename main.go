6d3d0eb0ab049c04f620ad2f92c156b31e79bcd4
Héctor Jorge Morales Arch <hjma98@gmail.com>
2021-04-17 13:43:06 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+)
+
+// User: Structure to store users info
+type User struct{
+	Username string
+	Password string
+	Token string
+}
+// Global variable user. All functions are able to access to it
+var user User
+
+func main() {
+	// User and password registered
+	user.Username="PIPO"
+	user.Password="123"
+	router := mux.NewRouter()
+	//All routes for API
+	//Need functions
+	router.HandleFunc("/Login",login)
+	router.HandleFunc("/Logout",logout)
+	router.HandleFunc("/Status",status)
+	router.HandleFunc("/Upload",addImage)
+	router.HandleFunc("/Token",token)
+	http.ListenAndServe(":5000",router)
+}
