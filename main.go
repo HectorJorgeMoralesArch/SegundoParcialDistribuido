16162c1b14f5722cf65df28f3176f18ac95ec19d
Jose Pablo <josepablolppl@gmail.com>
2021-04-19 05:23:26 +0000

    Login, Logout & Status done

--- main.go
+++ main.go
@@ -1,73 +1,163 @@
 package main
 
 import (
-	"fmt"
-	"crypto/rand"
-	"encoding/json"
-	"github.com/gorilla/mux"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-// User: Structure to store users info
-type User struct {
-	UID 	 string
-	Pass	 string
-	Token    string
-}
+// Logged: Structure to store logged in users info
+var online = make(map[string]string)
+
+var users = make(map[string]string)
 
 // Global variable user. All functions are able to access to it
-
-//Hola Hector
-
-var user User
-
 func main() {
-	// User and password registered
-	user.UID = "PIPO"
-	user.Pass = "123"
 	router := mux.NewRouter()
+	users["username"] = "password"
 	//All routes for API
 	//Need functions
-	router.HandleFunc("/Login", login)
-	router.HandleFunc("/Logout", logout)
-	router.HandleFunc("/Status", status)
-	router.HandleFunc("/Upload", addImage)
-	router.HandleFunc("/Token", token)
-	http.ListenAndServe(":5000", router)
+	router.HandleFunc("/login", login)
+	router.HandleFunc("/logout", logout)
+	router.HandleFunc("/status", status)
+	router.HandleFunc("/upload", addImage)
+	http.ListenAndServe(":8080", router)
 }
-<<<<<<< HEAD
-func login() {
 
-}
-func logout() {
-
-}
-func status() {
-
-}
-func addImage() {
-=======
-func login(w http.ResponseWriter, r *http.Request){
-	var userW User
-	json.NewDecoder(r.Body).Decode(&userW)
-	if user.UID == userW.UID && user.Pass == userW.Pass{
-		user.Token=token()
-	}else{
-		fmt.Println("Wrong Data")
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "application/json")
+	usr, pwd, ok := r.BasicAuth()
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+	{
+		"Username and Password are required"
 	}
+`
+		w.Write([]byte(msg))
+		return
+	}
+	if !checkPass(usr, pwd) {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+	{
+		"Invalid Username and/or Password"
+	}
+`
+		w.Write([]byte(msg))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	token := GenerateRandomString(10)
+	msg := `
+	{
+		"message": "Hi ` + usr + ` welcome to the DPIP System"
+		"token" "` + token + `"
+	}
+`
+	w.Write([]byte(msg))
+	online[token] = usr
+	return
 }
-func logout(w http.ResponseWriter, r *http.Request){
-	user.Token=""
-	fmt.Println("Logged out")
+
+func checkPass(usr string, pwd string) bool {
+	if p, ok := users[usr]; ok {
+		return p == pwd
+	}
+	return false
 }
-func status(w http.ResponseWriter, r *http.Request){
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if len(token) < 7 {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+	{
+		"Please enter a token" ` + token + `smiles
+	}
+`
+		w.Write([]byte(msg))
+		return
+	}
+	token = token[7:]
+	if !loggedIn(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+	{
+		"Please enter a valid token"
+	}
+`
+		w.Write([]byte(msg))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	msg := `
+	{
+		"message": "Bye ` + online[token] + `, your token has been revoked"
+	}
+`
+	w.Write([]byte(msg))
+	delete(online, token)
+	return
+}
+
+func loggedIn(token string) bool {
+	if _, ok := online[token]; ok {
+		return true
+	}
+	return false
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if len(token) < 7 {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+	{
+		"Please enter a token" ` + token + `
+	}
+`
+		w.Write([]byte(msg))
+		return
+	}
+	token = token[7:]
+	if !loggedIn(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+	{
+		"Please enter a valid token"
+	}
+`
+		w.Write([]byte(msg))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	msg := `
+	{
+		"message": "Hi ` + online[token] + `, the DPIP System is Up and Running "
+		"time": "` + time.Now().Format("2006-01-02 15:04:05") + `"
+	}
+`
+	w.Write([]byte(msg))
+	return
+}
+
+func addImage(w http.ResponseWriter, r *http.Request) {
 
 }
-func addImage(w http.ResponseWriter, r *http.Request){
->>>>>>> 18b11f2c8643e6420f665e468ac7a2523f33ef1e
 
-}
-func token() {
-	token:=make([]byte, 4)
-	return rand.Read(token)
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// GenerateRandomString generate a string of random characters of given length
+func GenerateRandomString(n int) string {
+	sb := strings.Builder{}
+	sb.Grow(n)
+	for i := 0; i < n; i++ {
+		idx := rand.Int63() % int64(len(letterBytes))
+		sb.WriteByte(letterBytes[idx])
+	}
+	return sb.String()
 }
